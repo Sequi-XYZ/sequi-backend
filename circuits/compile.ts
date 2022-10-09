7c5c732b231ac6e01f6e9df9aab90c51a3f62971
Colin Nielsen <colinrnielsen@gmail.com>
2022-10-09 08:50:54 -0500

    update circuits

--- circuits/compile.ts
+++ circuits/compile.ts
@@ -1,13 +1,16 @@
 import { resolve, join } from "path";
-import { compile } from "@noir-lang/noir_wasm";
+import { compile, acir_to_bytes } from "@noir-lang/noir_wasm";
 import { setup_generic_prover_and_verifier } from "@noir-lang/barretenberg/dest/client_proofs";
-import { writeFileSync } from "fs";
+import { writeFileSync, readFileSync } from "fs";
 
 async function generateSolVerifier() {
   // console.log(resolve(__dirname, "src/main.nr"));
   const compiled = compile(resolve(__dirname, "src/main.nr"));
   const acir = compiled.circuit;
-  console.log("ACIR", acir);
+  const buffer = readFileSync(resolve(__dirname, "src/main.nr"));
+  const acirbytes = acir_to_bytes(acir)
+  console.log(Buffer.from(acirbytes).toString("hex"));
+  // console.log("ACIR", acir);
 
   const [, verifier] = await setup_generic_prover_and_verifier(acir);
 
